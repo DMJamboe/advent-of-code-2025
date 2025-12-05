11e00a1ffbe3574218cbe03ecee9cad9d819e5cb
DMJamboe <sjevans400@gmail.com>
2025-12-05 07:52:26 +0000

    Solve Day 4 Part 2.

--- day-4/part-1.js
+++ day-4/part-1.js
@@ -1,6 +1,7 @@
 import { readFileSync } from "node:fs";
 
 const PAPER = "@";
+const REMOVED_PAPER = "x";
 const MAX_ADJACENT = 3;
 
 const dataFile = readFileSync("day-4/input.txt", "utf-8");
@@ -8,25 +9,38 @@ const parsedGrid = dataFile.split("\n").map((line) => line.split(""));
 
 let openPapers = 0;
 
-for (let y = 0; y < parsedGrid.length; y++) {
-  for (let x = 0; x < parsedGrid[y].length; x++) {
-    if (parsedGrid[y][x] === PAPER) {
-      const adjacencies = [
-        parsedGrid[y + 1]?.[x + 1],
-        parsedGrid[y + 1]?.[x],
-        parsedGrid[y + 1]?.[x - 1],
-        parsedGrid[y]?.[x + 1],
-        parsedGrid[y]?.[x - 1],
-        parsedGrid[y - 1]?.[x + 1],
-        parsedGrid[y - 1]?.[x],
-        parsedGrid[y - 1]?.[x - 1],
-      ];
-      const paperAdjacencies = adjacencies.filter((tile) => tile === PAPER);
-      if (paperAdjacencies.length <= MAX_ADJACENT) {
-        openPapers++;
+function replacePapers(grid) {
+  let replacedPapers = 0;
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === PAPER) {
+        const adjacencies = [
+          grid[y + 1]?.[x + 1],
+          grid[y + 1]?.[x],
+          grid[y + 1]?.[x - 1],
+          grid[y]?.[x + 1],
+          grid[y]?.[x - 1],
+          grid[y - 1]?.[x + 1],
+          grid[y - 1]?.[x],
+          grid[y - 1]?.[x - 1],
+        ];
+        const paperAdjacencies = adjacencies.filter((tile) => tile === PAPER);
+        if (paperAdjacencies.length <= MAX_ADJACENT) {
+          grid[y][x] = REMOVED_PAPER;
+          replacedPapers++;
+        }
       }
     }
   }
+
+  return replacedPapers;
 }
+
+let replacedPapers;
+do {
+  replacedPapers = replacePapers(parsedGrid);
+  openPapers += replacedPapers;
+} while (replacedPapers > 0);
 
 console.log("Papers free", openPapers);
